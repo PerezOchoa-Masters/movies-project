b7f123a2b16fcff2f07298ed19d0a29b4a3363d6
Antonio Perez Ochoa <antonio.perezochoa1@gmail.com>
2022-12-12 11:19:06 -0700

    Got the project started

--- js/movies.js
+++ js/movies.js
@@ -1,1 +1,66 @@
-fetch("https://glitch.com/edit/#!/exultant-tame-almandine").then(resp => resp.json()).then(data => console.log(data));
+"use strict";
+$(function() {
+
+fetch("https://bright-military-blizzard.glitch.me/movies")
+    .then(resp => resp.json())
+    .then(data => console.log(data));
+
+// ADD USER MOVIE
+$("#btn").click(function (e) {
+    e.preventDefault();
+    const newMovie = {
+        title: $("#title").val(),
+        rating: $("#rating").val(),
+        genre: $("#genre").val()
+    };
+    fetch("https://bright-military-blizzard.glitch.me/movies", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newMovie),
+    }).then(() => fetch("https://bright-military-blizzard.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
+});
+
+    // let movieList = [];
+    // movies.forEach(({ title, rating, genre }) => {
+    //     movieList.push(`${title}, ${rating}, ${genre}`);
+    // });
+    // let list = '<ul>';
+    // for(let movie of movieList) {
+    //     list += `<li>${movie}</li>`;
+    // }
+    // list += '</ul>';
+
+$("#movies").html(function (){
+    fetch("https://bright-military-blizzard.glitch.me/movies")
+        .then(resp => resp.json())
+        .then(data => console.log(data));
+
+    //document.getElementById("movies").innerHTML = list;
+
+});
+
+
+// // EDIT MOVIE
+// const editMovie = {
+//     title: "Be Kind Rewind",
+//     director: "Michael Gondry",
+//     release_date: 2008,
+//     cast: ["Yasiin Bey", "Jack Black", "Danny Glover"]
+// };
+// fetch("https://bright-military-blizzard.glitch.me/movies/{INSERT-USER-INPUT-HERE}", {
+//     method: 'PATCH',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(newMovie),
+// }).then(() => fetch("https://bright-military-blizzard.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
+
+
+// DELETE MOVIE
+// fetch("https://bright-military-blizzard.glitch.me/movies/5", {
+//     method: "DELETE"
+// }).then(() => fetch("https://bright-military-blizzard.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
+
+});
