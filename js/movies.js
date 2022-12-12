3b8aa6db50e984bba125fa61e7bab940d1340863
Antonio Perez Ochoa <antonio.perezochoa1@gmail.com>
2022-12-12 15:48:24 -0700

    Added functionality to delete

--- js/movies.js
+++ js/movies.js
@@ -1,81 +1,88 @@
 "use strict";
-$(function() {
-
-fetch("https://bright-military-blizzard.glitch.me/movies")
-    .then(resp => resp.json())
-    .then(data => console.log(data));
-
-//Loading Screen
-
-// ADD USER MOVIE (COMPLETED)
-$("#btn").click(function () {
-    const newMovie = {
-        title: $("#title").val(),
-        rating: $("#rating").val(),
-        genre: $("#genre").val()
-    };
-    fetch("https://bright-military-blizzard.glitch.me/movies", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newMovie),
-    }).then(() => fetch("https://bright-military-blizzard.glitch.me/movies"))
-        .then(resp => resp.json())
-        .then(movies => console.log(movies));
-
-});
-
-
-
-//Adding movies list to HTML
-
+$(function() { //IIFE open
 
     fetch("https://bright-military-blizzard.glitch.me/movies")
         .then(resp => resp.json())
-        .then(function(data) {
-            for (var i=0; i<data.length; i++) {
-                var row = $('<div class="row row-cols-2"><div class="col"><strong>' + data[i].title+ '</strong></div><div class="col">' + data[i].rating + '</div><div class="col">' + data[i].genre + '</div></div><button type="button" id="delete" class="btn-close" aria-label="Close"></button><br>');
-                $('#movies').append(row);
-            };
+        .then(data => console.log(data));
+
+    /////////////////Loading Screen////////////////
+    //
+    //
+    //////////////////////////////////////////////
+    renderMovies();
+
+    // ADD USER MOVIE (COMPLETED)
+    $("#btn").click(function () {
+        const newMovie = {
+            title: $("#title").val(),
+            rating: $("#rating").val(),
+            genre: $("#genre").val()
+        };
+        fetch("https://bright-military-blizzard.glitch.me/movies", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newMovie),
+        }).then(() => fetch("https://bright-military-blizzard.glitch.me/movies"))
+            .then(resp => resp.json())
+            .then(movies => console.log(movies));
+        renderMovies();
+    });
+
+    //Adding movies list to HTML
+
+    function renderMovies(){
+        fetch("https://bright-military-blizzard.glitch.me/movies")
+            .then(resp => resp.json())
+            .then(function(data) {
+                $("#movies").html('');
+                for (let i=0; i<data.length; i++) {
+                    let row = '<div class="row row-cols-2">' +
+                                    '<div class="col">' + '<strong>' + data[i].title+ '</strong>' + '</div>' +
+                                    '<div class="col">' + data[i].rating + '</div>' +
+                                    '<div class="col">' + data[i].genre + '</div>' +
+                                    '<button type="button" data-id="' + data[i].id +'" class="delete btn-close" aria-label="Close"></button>' +
+                                '</div><br>';
+                    $('#movies').append(row);
+                };
+                $(".delete").click(function (){
+                   let id = $(this).data("id")
+                   console.log(id)
+                    deleteMovie(id)
+                });
+            });
+    };
 
 
-
-    // let movieList = [];
-    // movies.forEach(({ title, rating, genre }) => {
-    //     movieList.push(`${title}, ${rating}, ${genre}`);
-    // });
-    // let list = '<ul>';
-    // for(let movie of movieList) {
-    //     list += `<li>${movie}</li>`;
-    // }
-    // list += '</ul>';
-
-
-    //document.getElementById("movies").innerHTML = list;
-
-});
+    // DELETE MOVIE
+    function deleteMovie(id){
+        let fetchOptions = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+        fetch(`https://bright-military-blizzard.glitch.me/movies/${id}`, fetchOptions)
+            .then((resp) => console.log("Deleted " + id, resp ))
+        renderMovies()
+    }
 
 
-// // EDIT MOVIE
-// const editMovie = {
-//     title: "Be Kind Rewind",
-//     director: "Michael Gondry",
-//     release_date: 2008,
-//     cast: ["Yasiin Bey", "Jack Black", "Danny Glover"]
-// };
-// fetch("https://bright-military-blizzard.glitch.me/movies/{INSERT-USER-INPUT-HERE}", {
-//     method: 'PATCH',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(newMovie),
-// }).then(() => fetch("https://bright-military-blizzard.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
+    // // EDIT MOVIE
+    // const editMovie = {
+    //     title: "Be Kind Rewind",
+    //     director: "Michael Gondry",
+    //     release_date: 2008,
+    //     cast: ["Yasiin Bey", "Jack Black", "Danny Glover"]
+    // };
+    // fetch("https://bright-military-blizzard.glitch.me/movies/{INSERT-USER-INPUT-HERE}", {
+    //     method: 'PATCH',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(newMovie),
+    // }).then(() => fetch("https://bright-military-blizzard.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
 
 
-// DELETE MOVIE
-// fetch("https://bright-military-blizzard.glitch.me/movies/5", {
-//     method: "DELETE"
-// }).then(() => fetch("https://bright-military-blizzard.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
-
-});
+}); //IIFE close
