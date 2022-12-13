753494ae931556a8b20e61c7cd2fee309daea988
Antonio Perez Ochoa <antonio.perezochoa1@gmail.com>
2022-12-13 14:47:53 -0700

    added loading icon and auto-reload information

--- js/movies.js
+++ js/movies.js
@@ -1,16 +1,38 @@
 "use strict";
+
 $(function() { //IIFE open
 
-    /////////////////Loading Screen////////////////
-    $(window).load(function() {      //Do this when the window has loaded
-        $(".loader").fadeOut("slow");  //Fade out the loader div
+    fetch("https://bright-military-blizzard.glitch.me/movies")
+        .then(resp => resp.json())
+        .then(data => console.log(data));
 
-        fetch("https://bright-military-blizzard.glitch.me/movies")
-            .then(resp => resp.json())
-            .then(data => console.log(data));
-        renderMovies();
-    });
 
+    //Update movies list HTML
+    function renderMovies(){
+        // setTimeout(() => {    //simulate slow page load
+            fetch("https://bright-military-blizzard.glitch.me/movies")
+                .then(resp => resp.json())
+                .then(function (data) {
+                    $(".loader").hide();
+                    $("#movies").html(''); //clear current html
+                    for(let i = 0; i < data.length; i++){
+                        let row = '<div class="row row-cols-5">' +
+                            '<div class="col">' + data[i].id + ')</div>' +
+                            '<div class="col">' + '<strong>' + data[i].title + '</strong>' + '</div>' +
+                            '<div class="col">Rating: ' + data[i].rating + '/5</div>' +
+                            '<div class="col">' + data[i].genre + '</div>' +
+                            '<button type="button" data-id="' + data[i].id + '" class="delete btn-close" aria-label="Close"></button>' +
+                            '</div><br>';
+                        $('#movies').append(row);
+                    } //for loop close
+                    $(".delete").click(function () {
+                        let id = $(this).data("id")
+                        console.log(id)
+                        deleteMovie(id)
+                    });
+                }); //close .then
+        // },3000);    //close of load simulation
+    }; //close renderMovies
 
 
     // ADD USER MOVIE (COMPLETED)
@@ -28,38 +50,13 @@ $(function() { //IIFE open
             body: JSON.stringify(newMovie),
         }).then(() => fetch("https://bright-military-blizzard.glitch.me/movies"))
             .then(resp => resp.json())
-            .then(movies => console.log(movies));
-        renderMovies();
+            .then(movies => {
+                console.log(movies)
+                renderMovies()});
+
     });
 
-    //Update movies list HTML
-    function renderMovies(){
-        fetch("https://bright-military-blizzard.glitch.me/movies")
-            .then(resp => resp.json())
-            .then(function(data) {
-                $("#movies").html(''); //clear current html
-                for (let i=0; i<data.length; i++) {
-                    let row = '<div class="row row-cols-5">' +
-                                    '<div class="col">' + data[i].id + ')</div>' +
-                                    '<div class="col">' + '<strong>' + data[i].title+ '</strong>' + '</div>' +
-                                    '<div class="col">Rating: ' + data[i].rating + '/5</div>' +
-                                    '<div class="col">' + data[i].genre + '</div>' +
-                                    '<button type="button" data-id="' + data[i].id +'" class="delete btn-close" aria-label="Close"></button>' +
-                                '</div><br>';
-                                $('#movies').append(row);
-                } //for loop close
-                $(".delete").click(function (){
-                   let id = $(this).data("id")
-                   console.log(id)
-                    deleteMovie(id)
-                });
-
-            }); //close .then
-
-    }; //close renderMovies
-
-
-    // DELETE MOVIE
+    // DELETE MOVIE (needs reload)
     function deleteMovie(id){
         let fetchOptions = {
             method: 'DELETE',
@@ -68,8 +65,10 @@ $(function() { //IIFE open
             },
         }
         fetch(`https://bright-military-blizzard.glitch.me/movies/${id}`, fetchOptions)
-            .then((resp) => console.log("Deleted " + id, resp ))
-        renderMovies();
+            .then((resp) => {
+                console.log("Deleted " + id, resp )
+                renderMovies();
+            })
     };
 
 
@@ -90,11 +89,14 @@ $(function() { //IIFE open
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(editMovie)
-        }).then(() => fetch("https://bright-military-blizzard.glitch.me/movies")).then(resp => resp.json()).then(movies => console.log(movies));
-
-        renderMovies();
+        }).then(() => fetch("https://bright-military-blizzard.glitch.me/movies"))
+            .then(resp => resp.json())
+            .then(movies => {
+                console.log(movies)
+                renderMovies();
+            });
     });
 
-
+    renderMovies();
 
 }); //IIFE close
